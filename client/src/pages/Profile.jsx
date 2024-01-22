ede4c9f5369aedee8f4941cadb5252c46dee9db6
Erfan Heshmati <heshmati74@gmail.com>
2024-01-22 15:07:55 +0330

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,66 @@
 import { useSelector } from "react-redux"
+import { useRef, useState, useEffect } from "react"
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage"
+import { app } from "../firebase"
+import { TbCameraPlus } from "react-icons/tb";
 
 export default function Profile() {
     const { currentUser } = useSelector((state) => state.user)
+    const fileRef = useRef(null)
+    const [file, setFile] = useState(undefined)
+    const [filePercentage, setFilePercentage] = useState(0)
+    const [fileUploadError, setFileUploadError] = useState(false)
+    const [formData, setFormData] = useState({})
+
+    // firebase storage:
+    // allow read;
+    //   allow write: if
+    //   request.resource.size < 2 * 1024 * 1024 &&
+    //   request.resource.contentType.matches('image/.*')
+
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file)
+        }
+    }, [file])
+
+    const handleFileUpload = (file) => {
+        const storage = getStorage(app)
+        const fileName = new Date().getTime() + file.name
+        const storageRef = ref(storage, fileName)
+        const uploadTask = uploadBytesResumable(storageRef, file)
+        uploadTask.on("state_changed", (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            setFilePercentage(Math.round(progress))
+        },
+            (error) => {
+                setFileUploadError(true)
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setFormData({ ...formData, avatar: downloadURL })
+                })
+            })
+    }
 
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
             <form className="flex flex-col gap-4">
-                <img src={currentUser.avatar} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2" />
+                <input type="file" ref={fileRef} hidden accept="image/*" onChange={(e) => setFile(e.target.files[0])} />
+                <div className="relative self-center">
+                    <img src={formData.avatar || currentUser.avatar} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer mt-2" onClick={() => fileRef.current.click()} />
+                    <TbCameraPlus className="absolute bottom-0 right-0 w-6 h-6 text-gray-600 bg-white cursor-pointer rounded-lg" onClick={() => fileRef.current.click()} />
+                </div>
+                <p className="text-center">
+                    {fileUploadError ?
+                        (<span className="text-red-600">Image upload failed! (Image must be less than 2MB)</span>) :
+                        filePercentage > 0 && filePercentage < 100 ?
+                            (<span className="text-slate-700">{`Uploading ${filePercentage}%`}</span>) :
+                            filePercentage === 100 ?
+                                (<span className="text-green-600">Image uploaded successfully</span>) : ""
+                    }
+                </p>
                 <input type="text" placeholder="Username" className="border p-3 rounded-lg" id="username" />
                 <input type="text" placeholder="Email" className="border p-3 rounded-lg" id="email" />
                 <input type="text" placeholder="Password" className="border p-3 rounded-lg" id="password" />
