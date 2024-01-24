38f68397001c369f41ab96126eb32e83ad1de8a8
Erfan Heshmati <heshmati74@gmail.com>
2024-01-24 16:18:00 +0330

    add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -16,6 +16,7 @@ export default function Search() {
     const navigate = useNavigate()
     const [loading, setLoading] = useState(false)
     const [listings, setListings] = useState([])
+    const [showMore, setShowMore] = useState(false)
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search)
@@ -40,9 +41,16 @@ export default function Search() {
 
         const fetchListings = async () => {
             setLoading(true)
+            setShowMore(false)
             const searchQuery = urlParams.toString()
             const response = await fetch(`/api/listing/get?${searchQuery}`)
             const data = await response.json()
+            if (data.length > 8) {
+                setShowMore(true)
+            }
+            else {
+                setShowMore(false)
+            }
             setListings(data)
             setLoading(false)
         }
@@ -78,6 +86,20 @@ export default function Search() {
         urlParams.set('order', sidebarData.order)
         const searchQuery = urlParams.toString()
         navigate(`/search?${searchQuery}`)
+    }
+
+    const onShowMoreClick = async () => {
+        const numberOfListings = listings.length
+        const startIndex = numberOfListings
+        const urlParams = new URLSearchParams(location.search)
+        urlParams.set('startIndex', startIndex)
+        const searchQuery = urlParams.toString()
+        const response = await fetch(`/api/listing/get?${searchQuery}`)
+        const data = await response.json()
+        if (data.length < 9) {
+            setShowMore(false)
+        }
+        setListings([...listings, ...data])
     }
 
     return (
@@ -130,14 +152,15 @@ export default function Search() {
                     <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90">Search</button>
                 </form>
             </div>
-            <div className="flex-1">
-                <h1 className="text-3xl font-semibold p-3 text-slate-700 mt-5">Listing Results:</h1>
+            <div className="flex-1 mb-5">
+                <h1 className="text-3xl font-semibold p-3 text-slate-700 mt-5">Search results:</h1>
                 <div className="p-5 flex flex-wrap gap-4">
                     {!loading && listings.length === 0 && (<p className="text-xl text-slate-700">No listing found!</p>)}
                     {loading && (<p className="text-xl text-slate-700 text-center w-full">Loading...</p>)}
                     {!loading && listings && listings.map((listing) => (<ListingItem key={listing._id} listing={listing} />))}
+                    {showMore && (<button onClick={onShowMoreClick} className="text-green-700 hover:underline w-full p-1 mt-6">Show more</button>)}
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
